9ba5b8ad2b79385e7dbb9480edc37eb25e787ab5
charlenebatiste <batistecharlene@gmail.com>
2021-05-26 18:41:46 -0400

    addition feature working

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,17 +3,41 @@ import './App.css';
 
 
 class Calculator extends Component{
+
+  state={
+    inputOne: '',
+    inputTwo: '',
+    total: ''
+  }
+  setNum = (e, num) => {
+    this.setState({ [num]: e.target.value});
+  }
+  addNumbers = () => {
+    // console.log(`you want to add ${this.state.inputOne} and ${this.state.inputTwo}?`);
+    let yourTotal = parseInt(this.state.inputOne) + parseInt(this.state.inputTwo);
+    // console.log(yourTotal);
+    this.setState({
+      inputOne: '',
+      inputTwo: '',
+      total: yourTotal
+    })
+  }
   render(){
     return (
       <div className="container">
         <h1>Add with React!</h1>
 
         <div className="add">
-          <input type="text" />
+          <input type="number" 
+          placeholder="enter a number" 
+          value={this.state.inputOne} 
+          onChange={ (e) => this.setNum(e, 'inputOne') }/>
           <span>+</span>
-          <input type="text" />
-          <button>=</button>
-          <h3>Addition results go here!</h3>
+          <input type="number" 
+          placeholder="enter another number" value={this.state.inputTwo} 
+          onChange={ (e) => this.setNum(e, 'inputTwo') }/>
+          <button onClick={this.addNumbers}>=</button>
+          <h3>Your total is: {this.state.total}</h3>
         </div>
       </div>
     );
