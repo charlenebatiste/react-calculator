1c234e3326fb936830ec52b268381446930b2cc7
charlenebatiste <batistecharlene@gmail.com>
2021-05-26 16:44:09 -0400

    set up of class component

---
+++ src/Calculator.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Calculator extends Component{
+  render(){
+    return (
+      <div className="container">
+        <h1>Add with React!</h1>
+
+        <div className="add">
+          <input type="text" />
+          <span>+</span>
+          <input type="text" />
+          <button>=</button>
+          <h3>Addition results go here!</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Calculator;
