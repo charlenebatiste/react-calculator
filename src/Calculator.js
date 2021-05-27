e87e74457c51d675a69beb7629fdfcfef780f623
charlenebatiste <batistecharlene@gmail.com>
2021-05-26 20:31:19 -0400

    complete react calc

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,13 +5,35 @@ import './App.css';
 class Calculator extends Component{
 
   state={
+    operator: '', 
     inputOne: '',
     inputTwo: '',
     total: ''
   }
+
+  setOperator = (e, op) => {
+    this.setState({ [op]: e.target.value});
+  }
+
   setNum = (e, num) => {
     this.setState({ [num]: e.target.value});
   }
+
+  calculate = () => {
+    if (this.state.operator === 'add') {
+      this.addNumbers()
+    } 
+    if (this.state.operator === 'subtract') {
+      this.subtractNumbers()
+    }
+    if (this.state.operator === 'multiply') {
+      this.multiplyNumbers()
+    } 
+    if (this.state.operator === 'divide'){
+      this.divideNumbers()
+    }
+  }
+
   addNumbers = () => {
     // console.log(`you want to add ${this.state.inputOne} and ${this.state.inputTwo}?`);
     let yourTotal = parseInt(this.state.inputOne) + parseInt(this.state.inputTwo);
@@ -22,23 +44,51 @@ class Calculator extends Component{
       total: yourTotal
     })
   }
+  subtractNumbers = () => {
+    let yourTotal = parseInt(this.state.inputOne) - parseInt(this.state.inputTwo);
+    this.setState({
+      inputOne: '',
+      inputTwo: '',
+      total: yourTotal
+    })
+  }
+  multiplyNumbers = () => {
+    let yourTotal = parseInt(this.state.inputOne) * parseInt(this.state.inputTwo);
+    this.setState({
+      inputOne: '',
+      inputTwo: '',
+      total: yourTotal
+    })
+  }
+  divideNumbers = () => {
+    let yourTotal = parseInt(this.state.inputOne) / parseInt(this.state.inputTwo);
+    this.setState({
+      inputOne: '',
+      inputTwo: '',
+      total: yourTotal
+    })
+  }
   render(){
     return (
       <div className="container">
-        <h1>Add with React!</h1>
-
-        <div className="add">
-          <input type="number" 
-          placeholder="enter a number" 
-          value={this.state.inputOne} 
-          onChange={ (e) => this.setNum(e, 'inputOne') }/>
-          <span>+</span>
-          <input type="number" 
-          placeholder="enter another number" value={this.state.inputTwo} 
-          onChange={ (e) => this.setNum(e, 'inputTwo') }/>
-          <button onClick={this.addNumbers}>=</button>
-          <h3>Your total is: {this.state.total}</h3>
-        </div>
+            <h1>Calculate with React!</h1>
+            <h2>Select your operator, input your numbers and go!</h2>
+        <div className="calculate">
+            <input type="number" value={this.state.inputOne} onChange={ (e) => this.setNum(e, 'inputOne') }/>
+            <div className="calculateButtons">
+                  <button value='add' onClick={ (e) => this.setOperator(e, 'operator')}> + </button>
+                  <button value='subtract' onClick={ (e) => this.setOperator(e, 'operator')}> - </button>
+                  <button value='multiply' onClick={ (e) => this.setOperator(e, 'operator')}> * </button>
+                  <button value='divide' onClick={ (e) => this.setOperator(e, 'operator')}> / </button>
+            </div>
+            <input type="number"  value={this.state.inputTwo} onChange={ (e) => this.setNum(e, 'inputTwo') }/>
+            <div className="calculateButtons">
+              <button onClick={this.calculate}>=</button>
+            </div>
+          </div>
+          <div>
+            <h3>Your total is: {this.state.total}</h3>
+          </div>
       </div>
     );
   }
